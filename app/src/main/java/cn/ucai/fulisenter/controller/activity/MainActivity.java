2a121f0a4aec958bd943673d8a0f40114ddee91c
jwl1 <1305195816@qq.com>
2017-01-10 20:24:04 +0800

    Butter knife

--- app/src/main/java/cn/ucai/fulisenter/controller/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulisenter/controller/activity/MainActivity.java
@@ -1,41 +1,60 @@
 package cn.ucai.fulisenter.controller.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RadioButton;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulisenter.R;
 
 
 public class MainActivity extends AppCompatActivity {
-    int  index, currentIndex;
-    RadioButton rbNewGoods,rbBoutique,rbCategory,rbCart, rbPersonl;
+    int index, currentIndex;
+    RadioButton rbNewGoods, rbBoutique, rbCategory, rbCart, rbPersonl;
     RadioButton[] rbs = new RadioButton[5];
+    @BindView(R.id.layout_new_good)
+    RadioButton layoutNewGood;
+    @BindView(R.id.layout_boutique)
+    RadioButton layoutBoutique;
+    @BindView(R.id.layout_category)
+    RadioButton layoutCategory;
+    @BindView(R.id.layout_cart)
+    RadioButton layoutCart;
+    @BindView(R.id.layout_personal_center)
+    RadioButton layoutPersonalCenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rbNewGoods = (RadioButton) findViewById(R.id.layout_new_good);
-        rbBoutique = (RadioButton) findViewById(R.id.layout_boutique);
-        rbCategory = (RadioButton) findViewById(R.id.layout_category);
-        rbCart = (RadioButton) findViewById(R.id.layout_cart);
-        rbPersonl = (RadioButton) findViewById(R.id.layout_personal_center);
+        ButterKnife.bind(this);
 
-
+        rbs[0] = layoutNewGood;
+        rbs[1] = layoutBoutique;
+        rbs[2] = layoutCategory;
+        rbs[3] = layoutCart;
+        rbs[4] = layoutPersonalCenter;
     }
 
     public void onCheckedChange(View view) {
         switch (view.getId()) {
             case R.id.layout_new_good:
+                index = 0;
                 break;
             case R.id.layout_boutique:
+                index = 1;
                 break;
             case R.id.layout_category:
+                index = 2;
                 break;
             case R.id.layout_cart:
+                index = 3;
                 break;
             case R.id.layout_personal_center:
+                index = 4;
                 break;
 
         }
@@ -46,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setRadioStatus() {
-        for (int i=0;i<rbs.length;i++) {
+        for (int i = 0; i < rbs.length; i++) {
             if (index != i) {
                 rbs[i].setChecked(false);
             } else {
@@ -54,6 +73,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         currentIndex = index;
+
+    }
+
+    @OnClick(R.id.mainLayout)
+    public void onClick() {
 
     }
 }
